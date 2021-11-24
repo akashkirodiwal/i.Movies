cf97efa06cbecd4ecc55092ed2c2605113e6a182
ee17b031-iittp <ee17b031@iittp.ac.in>
2021-11-24 19:52:44 +0530

    added ticket history and booking facility completely done

--- movies/views.py
+++ movies/views.py
@@ -16,23 +16,21 @@ def booking(request):
 
 def movie_selected(request, movie_title):
     context = {'screening': Screening.objects.filter(movie_id__title__contains=movie_title)}
-    no_of_seats = 0
-    if request.method == 'POST':
-        no_of_seats = request.POST['no_of_seats']
-        print(no_of_seats)
-
     return render(request, 'movies/screening.html', context)
 
 
 def ticket_booked(request, screen_pk):
-    seats = 5
-    current_user = request.user
-    user = User.objects.filter(username=current_user).first()
-    screen = Screening.objects.filter(pk=screen_pk).first()
-    screen.available_seats -= seats
-    screen.save()
-    for i in range(seats):
-        ticket = Ticket(user=user, screening=screen)
-        ticket.save()
-    context = {'tickets': Ticket.objects.filter(user=current_user)}
+    if request.method == 'POST':
+        no_of_seats = request.POST['no_of_seats']
+        current_user = request.user
+        user = User.objects.filter(username=current_user).first()
+        screen = Screening.objects.filter(pk=screen_pk).first()
+        screen.available_seats -= int(no_of_seats)
+        screen.save()
+        for i in range(int(no_of_seats)):
+            ticket = Ticket(user=user, screening=screen)
+            ticket.save()
+        context = {'tickets': Ticket.objects.filter(user=current_user)}
     return render(request, 'users_profile/profile.html', context)
+
+
