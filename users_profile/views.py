fbbb366540dfa2fbd7c5726ee11562867c2ee2fe
Mohini singhal <monisinghal30@gmail.com>
2021-11-25 10:32:57 +0530

    views added

--- users_profile/views.py
+++ users_profile/views.py
@@ -17,12 +17,44 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account Created for {username}.You can Login Now!!')
-            return redirect('login')
-
+            email = form.cleaned_data.get('email')
+            us=User.objects.filter(username=username).first()
+            us.is_active=False
+            us.save()
+            return redirect('otp',username,email)
+            
     else:
         form=UserRegisterForm()
     return render(request,'users_profile/register.html',{'form':form})
+
+
+def otp(request,username,email):
+        otp=random.randrange(100000,999999)
+        subject, from_email, to = 'welcome', 'aduser.movie30@gmail.com', email
+        html_content = f"Your email registered with {username}.Please Enter this OTP:{otp}"
+        msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        context={'otp':otp,'username':username}
+        return render(request,'users_profile/otp.html',context)
+
+
+def validate_otp(request):
+    usr_otp=request.POST.get('otp2','')
+    otp =request.POST.get('otp','')
+    username=request.POST.get('username','')
+    us=User.objects.filter(username=username).first()
+    if int(otp)==int(usr_otp):
+        us.is_active=True
+        us.save()
+        messages.success(request,f'Account Created for {username}')
+        return redirect('booking-home')
+    else:
+        us.delete()
+        messages.success(request,"OTP not matching TRY AGAIN")
+        return redirect('register')
+
+
 
 @login_required
 def profile(request):
